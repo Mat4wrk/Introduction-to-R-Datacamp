7e8641820d8c8de278ee6ecebcf1489945b4310f
Mat4wrk <77670765+Mat4wrk@users.noreply.github.com>
2021-04-26 00:58:34 +0700

    Create Apples and oranges.r

---
+++ 1.Intro to basics/Apples and oranges.r
@@ -0,0 +1,8 @@
+# Assign a value to the variable my_apples
+my_apples <- 5 
+my_oranges <- 6 
+# Fix the assignment of my_oranges
+my_apples + my_oranges
+
+# Create the variable my_fruit and print it out
+my_fruit <- my_apples + my_oranges
